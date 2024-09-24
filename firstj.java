28424f44e8b0200f2dbe124d88abd66380883592
Asmika-Panchal <asmikapanchal@gmail.com>
2024-09-25 00:27:15 +0530

    Create firstj.java

---
+++ firstj.java
@@ -0,0 +1,65 @@
+import java.util.*;
+
+public class firstj{
+    
+
+    public static void main (String args[]){
+        // System.out.print("Hello world\n");
+        // System.out.println("****");
+        // System.out.println("***");
+        // System.out.println("**");
+        // System.out.println("*");
+        // int a=10;
+        // int b=20;
+        // String name="Asmika";
+        // System.out.println(b);
+        // b=5;
+        // a=b;
+        // System.out.println(b);
+        // System.out.println(a);
+
+        // byte by=8;
+        // char ch='a';
+        // boolean var=false;
+        // double price=10.5;
+        // int num=25;
+        // short n=240;
+
+        //Sum of a and b
+        // int a=10;
+        // int b=5;
+        // int sum=a+b;
+        // System.out.println("Sum: "+sum);
+        
+        // Scanner sc=new Scanner(System.in);
+        // String input =sc.next();
+        // System.out.println(input);
+      
+        // float fl =sc.nextFloat();
+        // System.out.println(fl);
+
+        //sum  and product of a and b using input
+// Scanner sc=new Scanner(System.in);
+// int a=sc.nextInt();
+// int b=sc.nextInt();
+// int sum=a+b;
+// int product=a*b;
+// System.out.println("Sum: "+sum);
+// System.out.println("Product: "+product);
+
+//area of circle
+// Scanner sc=new Scanner(System.in);
+// float radius=sc.nextFloat();
+// float area=3.14f*radius*radius;
+// System.out.println("Area= "+area);
+
+//typecasting
+// float a=25.6f;
+// int b=(int)a;
+//        System.out.println(b);
+//        char ch='a';
+//        int no=ch;
+//        System.out.println(no);
+    }
+    }
+    //boilerplate code
