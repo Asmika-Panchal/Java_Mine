14b4cfc5291011ba42714ac35791747fe0106ed1
Asmika-Panchal <asmikapanchal@gmail.com>
2024-09-25 22:51:35 +0530

    c

---
+++ conditionSt.java
@@ -0,0 +1,18 @@
+public class conditionSt {
+    public static void main(String[] args) {
+        // int age=22;
+        // if(age>=18){
+        //     System.out.println("adult");
+        // }else{
+        //     System.out.println("minor");
+        // }
+
+        int a=1, b=3;
+        if(a>=b){
+            System.out.println("a is greater than b");
+        }
+        else{
+            System.out.println("b is greater than a");
+        }
+    }
+}
---
+++ operators.java
@@ -0,0 +1,44 @@
+public class operators{
+    public static void main(String args[]){
+        //binary op
+        // int a=10, b=5;
+        // System.out.println("add: " +(a+b));
+        // System.out.println("subtract: " +(a-b));
+        // System.out.println("multiply: " +(a*b));
+        // System.out.println("divide: " +(a/b));
+        // System.out.println("modulo: " +(a%b));
+
+        // //pre increment -unary
+        // int a=10;
+        // int b=++a;
+        // System.out.println(a);
+        // System.out.println(b);
+
+        //post increment -unary
+        // int a=10;
+        // int b=a++;
+        // System.out.println(a);
+        // System.out.println(b);
+
+        // //relational op
+        //  int a=10, b=5;
+        //  System.out.println("a==b: " +(a==b));
+        //  System.out.println("a!=b: " +(a!=b));
+        // System.out.println("a>b: " +(a>b));
+        // System.out.println("a<b: " +(a<b));
+
+        //logical op
+        // System.out.println((3>2) && (5<0));
+        // System.out.println((3>2) || (5<0));
+        // System.out.println( !(3<2));
+
+        //assignment op
+        int c=14;
+        c +=10;
+        int d=14;
+        d *=10;
+        System.out.println(c);
+        System.out.println(d);
+
+    }
+}
